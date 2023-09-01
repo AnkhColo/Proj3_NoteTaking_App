02327eb7a7669f3ff3c1e92eaa64c352574661e4
“Colin <muriithicolin98@gmail.com>
2023-09-02 00:08:06 +0300

    NotedJS

---
+++ Notes.js
@@ -0,0 +1,6 @@
+const noteArea = document.getElementById('noteArea');
+const AddNoteButton = document.getElementById('addNote');
+const noteList = document.getElementById('noteList');
+const modalContent = document.getElementById('modal');
+const closeButton = document.getElementById('closeButton');
+const modalNoteContent = document.getElementById('modalNoteContent');
