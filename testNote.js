03d6cb78e6cc959a09d18becfcfd5a61f313f558
“Colin <muriithicolin98@gmail.com>
2023-09-08 06:14:41 +0300

    DOne!

--- testNote.js
+++ testNote.js
@@ -3,7 +3,7 @@ const noteText = document.getElementById("noteText");
 const noteList = document.getElementById("noteList");
 const modal = document.getElementById("modal");
 const modalNoteContent = document.getElementById("modalNoteContent");
-const closeButton = document.getElementById("detailsButton");
+const closeButton = document.getElementById("closeButton");
 
 addNoteButton.addEventListener("click", addNote);
 noteList.addEventListener("click", openModal);
@@ -11,3 +11,41 @@ closeButton.addEventListener("click", closeModal);
 
 function addNote(){
     const noteContent = noteText.value;
+
+    if (noteContent.trim() !==""){
+        const note = document.createElement("div");
+        note.classList.add("note");
+
+        note.textContent = noteContent.length > 80 ? noteContent.substring(0, 80) + "..." : noteContent;
+
+        detailsButton = document.createElement("button");
+        detailsButton.textContent = "Details";
+        detailsButton.classList.add("detailsButton");
+
+        note.dataset.fullContent = noteContent;
+
+        note.appendChild(detailsButton);
+        noteList.appendChild(note);
+        noteText.value = "";
+    }
+}
+
+function openModal(event){
+    if (event.target.classList.contains ("detailsButton")){
+        const note = event.target.parentNode;
+        const content = note.dataset.fullContent;
+        modal.style.display = "block";
+        modalNoteContent.textContent = content; 
+    }
+}
+
+function closeModal(){
+    modal.style.display = "none";
+    modalNoteContent.textContent = "";
+}
+
+window.addEventListener('click', (event) => {
+    if (event.target === modal){
+        closeModal();
+    }
+});
